3d7233411ab815677b2ea75d33e7a8412f969ff8
Jose <maytadazajos@correo.com>
2023-04-20 23:52:27 -0400

    Jose v25

--- Views/Usuarios/usuarios.php
+++ Views/Usuarios/usuarios.php
@@ -35,6 +35,14 @@
                                 </thead>
                                 <tbody>
 
+                                    <tr>
+                                    <td>Tiger Nixon</td>
+                                    <td>System Architect</td>
+                                    <td>Edinburgh</td>
+                                    <td>61</td>
+                                    <td>2011-04-25</td>
+                                    </tr>
+
                                 </tbody>
                                 </table>
                             </div>
