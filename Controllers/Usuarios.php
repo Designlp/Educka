420ce20b4c64e4fb7583f8d65e914d787268a482
Jose <maytadazajos@correo.com>
2023-04-20 21:57:19 -0400

    Jose v14

---
+++ Controllers/Usuarios.php
@@ -0,0 +1,19 @@
+<?php 
+    class Usuarios extends Controllers{
+        public function __construct() {
+            parent::__construct();
+        }
+        public function Usuarios(){
+
+            $data['page_id'] = 1;
+            $data['page_tag'] = "Usuarios";
+            $data['page_title']= "Pagina Principal";
+            $data['page_name'] = "usuarios";
+            session_start();
+            $this->views->getview($this,"usuarios",$data);
+            
+        }
+
+
+    }
+?>
--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -55,14 +55,7 @@
     <body class="layout-mini layout-mini">
 
         <div class="preloader">
-            <div class="sk-chase">
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-            </div>
+         
 
             <!-- <div class="sk-bounce">
     <div class="sk-bounce-dot"></div>
