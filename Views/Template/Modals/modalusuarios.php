23b80c1ebbe3c05738da1d90a7aaf4776d28f2b8
Jose <maytadazajos@correo.com>
2023-04-21 22:43:13 -0400

    Jose v46

--- Views/Template/Modals/modalusuarios.php
+++ Views/Template/Modals/modalusuarios.php
@@ -8,40 +8,51 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-      <div class="tile">
-           
-            <div class="tile-body">
-              <form id="formrol" name="formrol">
-              <input id="idrol" name="idrol" type="hidden" value="">
-                
-                <div class="form-group">
-                  <label class="control-label">Nombre</label>
-                  <input class="form-control" id="txtnombre" name="txtnombre" type="text" placeholder="Nombre del Rol" required="">
-                </div>
-         
-                <div class="form-group">
-                  <label class="control-label">Descripcion</label>
-                  <textarea class="form-control" id="txtdescripcion" name="txtdescripcion" rows="2" placeholder="Descripcion de Rol"></textarea>
-                </div>
-                
-                <div class="form-group">
-                    <label for="exampleSelect1">Estado</label>
-                    <select class="form-control" id="liststatus" name="liststatus" placeholder="Estado">
-                      <option value="1">Activo</option>
-                      <option value="2">Inactivo</option>
-                      
-                    </select>
-                </div>
-                <div class="tile-footer">
-                    <button id="btnactionform" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btntext">Guardar</span></button>&nbsp;&nbsp;&nbsp;<a class="btn btn-secondary" href="#" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</a>
-                </div>
-              </form>
-            </div>
-            
-          </div>
-      </div>
-      
+      <div class="col-lg-8 d-flex align-items-center">
+                            <div class="flex" style="max-width: 100%">
+
+                                <div class="was-validated">
+                                    <div class="form-row">
+                                        <div class="col-12 col-md-6 mb-3">
+                                            <label class="form-label" for="validationSample01">First name</label>
+                                            <input type="text" class="form-control" id="validationSample01" placeholder="First name" value="Mark" required="">
+                                            <div class="invalid-feedback">Please provide a first name.</div>
+                                            <div class="valid-feedback">Looks good!</div>
+                                        </div>
+                                        <div class="col-12 col-md-6 mb-3">
+                                            <label class="form-label" for="validationSample02">Last name</label>
+                                            <input type="text" class="form-control" id="validationSample02" placeholder="Last name" value="Otto" required="">
+                                            <div class="invalid-feedback">Please provide a last name.</div>
+                                            <div class="valid-feedback">Looks good!</div>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col-12 col-md-6 mb-3">
+                                            <label class="form-label" for="validationSample03">City</label>
+                                            <input type="text" class="form-control" id="validationSample03" placeholder="City" required="">
+                                            <div class="invalid-feedback">Please provide a valid city.</div>
+                                            <div class="valid-feedback">Looks good!</div>
+                                        </div>
+                                        <div class="col-12 col-md-6 mb-3">
+                                            <label class="form-label" for="validationSample04">State</label>
+                                            <input type="text" class="form-control" id="validationSample04" placeholder="State" required="">
+                                            <div class="invalid-feedback">Please provide a valid state.</div>
+                                            <div class="valid-feedback">Looks good!</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox">
+                                        <input class="custom-control-input" type="checkbox" value="" id="invalidCheck01" required="" checked="">
+                                        <label class="custom-control-label" for="invalidCheck01">
+                                            Agree to terms and conditions
+                                        </label>
+                                    </div>
+                                </div>
+                                <button class="btn btn-primary" type="submit">Submit</button>
+
+                            </div>
+                        </div>
     </div>
   </div>
 </div>
