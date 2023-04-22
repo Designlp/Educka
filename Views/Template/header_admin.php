e80442ac7b0553ac62a831a2efa4688bc60c4071
Jose <maytadazajos@correo.com>
2023-04-22 18:18:15 -0400

    Jose v65

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -16,7 +16,7 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700%7CRoboto:400,500%7CExo+2:600&display=swap"
               rel="stylesheet">
-         <link rel="stylesheet" type="text/css" href="<?= media(); ?>/css/sweetalert.css">
+         <link rel="stylesheet" type="text/css" href="<?= media(); ?>/css/main.css">
         <!-- Preloader -->
         <link type="text/css"
               href="<?= media() ?>/vendor/spinkit.css"
