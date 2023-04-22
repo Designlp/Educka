82c021e064aeaf25fc92fa9da395676ec4e4dfaa
Jose <maytadazajos@correo.com>
2023-04-22 18:05:12 -0400

    Jose v58

--- Assets/js/functions/functionsusuarios.js
+++ Assets/js/functions/functionsusuarios.js
@@ -36,6 +36,7 @@ document.addEventListener("DOMContentLoaded",function(){
         request.send(formdata);
         request.onreadystatechange =function(){
             if(request.readyState == 4 && request.status==200){
+                alert();
                 console.log(request.responseText);
                 var obdata=JSON.parse(request.responseText);
                 console.log(obdata);
