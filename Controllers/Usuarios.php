0e3f20d2ba92e7d33c30d96d5a2281bff15ae407
Jose <maytadazajos@correo.com>
2023-04-27 17:21:28 -0400

    Jose Login

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -2,6 +2,10 @@
     class Usuarios extends Controllers{
         public function __construct() {
             parent::__construct();
+            session_start();
+            if(empty($_SESSION['login'])){
+                header('Location: '.base_url()."/login");
+            }
         }
         
         //Visualizacion
