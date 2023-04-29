0278efcebd213b42054a819d7404f9ea41664fb4
Jose <maytadazajos@correo.com>
2023-04-29 13:27:10 -0400

    Jose index

--- Views/Template/footer_principal.php
+++ Views/Template/footer_principal.php
@@ -1,97 +1,102 @@
+            <!-- Footer -->
 
-<footer id="footer-3-cols" class="site-footer">
-    <div id="footer-container">
-        <div id="footer-grid">
-            <div id="left-footer-section" class="footer-section">
+            <div class="bg-white border-top-2 mt-auto">
+                    <div class="container page__container page-section d-flex flex-column">
+                        <p class="text-70 brand mb-24pt">
+                            <img class="brand-icon"
+                                 src="<?= media() ?>/images/logo/black-70@2x.png"
+                                 width="30"
+                                 alt="Luma"> Luma
+                        </p>
+                        <p class="measure-lead-max text-50 small mr-8pt">Luma is a beautifully crafted user interface for modern Education Platforms, including Courses & Tutorials, Video Lessons, Student and Teacher Dashboard, Curriculum Management, Earnings and Reporting, ERP, HR, CMS, Tasks, Projects, eCommerce and more.</p>
+                        <p class="mb-8pt d-flex">
+                            <a href=""
+                               class="text-70 text-underline mr-8pt small">Terms</a>
+                            <a href=""
+                               class="text-70 text-underline small">Privacy policy</a>
+                        </p>
+                        <p class="text-50 small mt-n1 mb-0">Copyright 2019 &copy; All rights reserved.</p>
+                    </div>
+                </div>
 
-            <h3>Categories</h3>
-                <div class="footer-information">
-                    <div class="footer-information">
-                    <p><img src="https://img.icons8.com/ios-filled/12/999999/marker.png">30/20, Verkhy street, Moscow, Russia</p>
-                    <p><img src="https://img.icons8.com/ios-filled/12/999999/phone.png">7 (800) 555–35–35</p>
-                    <p><img src="https://img.icons8.com/ios-filled/12/999999/mail.png">noreply@reply.io</p>
-                    <p><img src="https://img.icons8.com/ios-filled/12/999999/clock.png">8:00 AM – 8:00 PM</p>
-                </div>
-                </div>
-            </div>
-            <div id="second-footer-section" class="footer-section">
-                <div class="footer-links">
-                    <h3>Informacion</h3>
-                    <ul>
-                        
-                        <li role="menuitem"><a href="#">Nosotros</a></li>
-                        <li role="menuitem"><a href="#">Ofertas</a></li>
-                        <li role="menuitem"><a href="#">Catalogo</a></li>
-                        <li role="menuitem"><a href="#">Carrito</a></li>
-                        <li role="menuitem"><a href="#">Contacto</a></li>
-                    </ul>
-                    
-                </div>
-                
+                <!-- // END Footer -->
+
             </div>
 
-            
-         
-            <div id="right-footer-section" class="footer-section">
-                <div class="footer-links">
-                    <h3>Contactos</h3>
-                    <ul>
-                        <li><a href="https://wa.me/61210301?text=¡Necesito+hablar+con+un+operador!" target="_blank">Jorge Flores </a></li>
-                        <li><a href="https://wa.me/68174137?text=¡Necesito+hablar+con+un+operador!" target="_blank">Jose Mayta </a></li>
-                        <li><a href="https://wa.me/76233755?text=¡Necesito+hablar+con+un+operador!" target="_blank">Rodrigo Chavez </a></li>
-                        <li><a href="https://wa.me/73742697?text=¡Necesito+hablar+con+un+operador!" target="_blank">Jhonatan Cordori </a></li>
-                        <li><a href="https://wa.me/70540655?text=¡Necesito+hablar+con+un+operador!" target="_blank">Mateo de la barra </a></li>
-                    </ul>
-                    
-                </div>
-            </div>
+            <!-- // END drawer-layout__content -->
 
-            <div id="right-footer-section" class="footer-section">
-                <div class="footer-links">
-                    <br><br>
-                    <ul>
-                        <li><a href="https://wa.me/75881611?text=¡Necesito+hablar+con+un+operador!" target="_blank">Fernando Baldi</a></li>
-                        <li><a href="https://wa.me/72083638?text=¡Necesito+hablar+con+un+operador!" target="_blank">Johan Arraya</a></li>
-                        <li><a href="https://wa.me/72083638?text=¡Necesito+hablar+con+un+operador!" target="_blank">Carlos Vallejos </a></li>
-                        <li><a href="https://wa.me/67046780?text=¡Necesito+hablar+con+un+operador!" target="_blank">Mateo Ascarrunz </a></li>
-                        <li><a href="https://wa.me/79530061?text=¡Necesito+hablar+con+un+operador!" target="_blank">Amos Paco </a></li>
-                    </ul>
-                    
-                </div>
-            </div>
-            
+            <!-- <?php require_once("nav_admin.php") ?>-->
+
         </div>
-        <div id="copyright-information">
-            <div id="footer-logo-section">
-                <div id="footer-logo">
-                    <img src="https://raw.githubusercontent.com/taviskaron/2-3-4-column-footers/main/img/logo.png" alt="">
-                </div></div>
-            <div id="copyright-text">
-            &copy; 2022 Romeo & Julieta 
-            </div>
-            <div id="social-buttons">
-                <img src="https://img.icons8.com/ios-filled/25/999999/facebook--v1.png"/><img src="https://img.icons8.com/ios-filled/25/999999/telegram-app.png"/><img src="https://img.icons8.com/ios-filled/25/999999/pinterest--v1.png"/><img src="https://img.icons8.com/ios-filled/25/999999/instagram--v1.png"/>
-            </div>
-        </div>
-    </div>
-</footer>
+
+        <!-- // END Drawer Layout -->
 
 
 
 
+        <script>
+            const baseurl="<?= base_url(); ?>";
+        </script>
+        <!-- jQuery -->
+        <script src="<?= media() ?>/vendor/jquery.min.js"></script>
+        <!-- jQuery personalisado-->
+        <!--  Descargar las librerias-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="<?= media() ?>/js/jquery-3.3.1.min.js"></script>
+        <script src="<?= media() ?>/js/popper.min.js"></script>
+        <script src="<?= media() ?>/js/bootstrap.min.js"></script>
+        <script src="<?= media() ?>/js/main.js"></script>
+        <script src="<?= media() ?>/js/plugins/pace.min.js"></script>
+        <script src="<?= media() ?>/js/plugins/bootstrap-notify.min.js"></script>
+        <script type="text/javascript" src="<?= media() ?>/js/plugins/sweetalert.min.js"></script>
+        <!-- Data table plugin-->
+        <script type="text/javascript" src="<?= media() ?>/js/plugins/jquery.dataTables.min.js"></script>
+        
+        <!-- Data table plugin-->
+        <!-- Permite crear tablas de datos atractivas y altamente funcionales-->
+        <script type="text/javascript" src="<?= media() ?>/js/plugins/dataTables.bootstrap.min.js"></script>
+  
+        <script type="text/javascript" src="<?= media() ?>/js/plugins/bootstrap-select.min.js"></script>
+        <script type="text/javascript" src="<?= media() ?>/js/plugins/moment.min.js"></script>
+        <!-- Bootstrap -->
+        <!-- El popper libreria qua muestra mensajes emmergentes -->
+        <!-- Esta Vendor
+        <script src="<?= media() ?>/vendor/popper.min.js"></script>
 
-    <script>
-        const baseurl="<?= base_url(); ?>";
-        const media="<?= media(); ?>";
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="<?=media();?>/js/script.min.js"></script>
-    
-    <script src="<?= media() ?>/js/printThis.js"></script>
-    <script type="text/javascript" src="<?= media() ?>/js/<?= $data['page_js'] ?>"></script>
+        <script src="<?= media() ?>/vendor/bootstrap.min.js"></script>-->
 
-    <script type="text/javascript" src="<?= media() ?>/js/plugins/sweetalert.min.js"></script>
-</body>
+        <!-- Perfect Scrollbar -->
+        <script src="<?= media() ?>/vendor/perfect-scrollbar.min.js"></script>
 
-</html>
+        <!-- DOM Factory -->
+        <script src="<?= media() ?>/vendor/dom-factory.js"></script>
+
+        <!-- MDK -->
+        <script src="<?= media() ?>/vendor/material-design-kit.js"></script>
+
+        <!-- App JS -->
+        <script src="<?= media() ?>/js/template/app.js"></script>
+
+        <!-- Preloader -->
+        <script src="<?= media() ?>/js/template/preloader.js"></script>
+
+        <!-- Sidebar Mini JS -->
+        <script src="<?= media() ?>/js/template/sidebar-mini.js"></script>
+        <script>
+            (function() {
+                'use strict';
+                // ENABLE sidebar menu tabs
+                $('.js-sidebar-mini-tabs [data-toggle="tab"]').on('click', function(e) {
+                    e.preventDefault()
+                    $(this).tab('show')
+                })
+                $('.js-sidebar-mini-tabs').on('show.bs.tab', function(e) {
+                    $('.js-sidebar-mini-tabs > .active').removeClass('active')
+                    $(e.target).parent().addClass('active')
+                })
+            })()
+        </script>
+
+
+        <script src="<?= media() ?>/js/functions/<?= $data['page_js'] ?>"></script>
+
