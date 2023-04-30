2d9225777bd050343e7df50d5dfead1fb10a53f6
Jose <maytadazajos@correo.com>
2023-04-30 19:58:27 -0400

    moises estu v19

--- Controllers/Estudiantes.php
+++ Controllers/Estudiantes.php
@@ -77,7 +77,7 @@
 
         //Insert 
         //Logica update como
-        public function setusuarios(){
+        public function setestudiantes(){
             if($_POST){	
             if(empty($_POST['txtnombre']) || empty($_POST['txtapellido']) || empty($_POST['txtcorreo']) )
             {
