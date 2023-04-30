e3e7168094a52d131e0539f6e9c2e4669b705d74
Jose <maytadazajos@correo.com>
2023-04-30 11:46:57 -0400

    Jose roles

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -25,7 +25,7 @@
                              data-perfect-scrollbar>
                             <ul class="nav flex-shrink-0 flex-nowrap flex-column sidebar-menu mb-0 js-sidebar-mini-tabs"
                                 role="tablist">
-                                <li class="sidebar-menu-item active"
+                                <li class="sidebar-menu-item "
                                     data-toggle="tooltip"
                                     data-title="Estudiantes"
                                     data-placement="right"
@@ -103,7 +103,7 @@
                                 </li>
                                 <li class="sidebar-menu-item"
                                     data-toggle="tooltip"
-                                    data-title="Estudiantes"
+                                    data-title="Usuarios"
                                     data-placement="right"
                                     data-container="body"
                                     data-boundary="window">
