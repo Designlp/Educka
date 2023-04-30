fa6f0049bc55d551feb3910ed357347a640e3f9d
Jose <maytadazajos@correo.com>
2023-04-30 12:20:06 -0400

    Jose roles

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -26,11 +26,7 @@
                             <ul class="nav flex-shrink-0 flex-nowrap flex-column sidebar-menu mb-0 js-sidebar-mini-tabs"
                                 role="tablist">
                                 <li class="sidebar-menu-item active"
-                                    data-toggle="tooltip"
-                                    data-title="Usuarios"
-                                    data-placement="right"
-                                    data-container="body"
-                                    data-boundary="window">
+                                 >
                                     <a class="sidebar-menu-button"
                                        href="#sm_components"
                                        data-toggle="tab"
