beee10d1144820fb6b49a670bf14396416a41e43
Jose <maytadazajos@correo.com>
2023-05-03 23:34:59 -0400

    Jose correcciones v4

--- Views/Template/Modals/modalcursos.php
+++ Views/Template/Modals/modalcursos.php
@@ -34,11 +34,7 @@
                 </div>
 
                 <div class="form-group">
-                <select id="listprivado" data-toggle="select" class="form-control select2-hidden-accessible" data-select2-id="select01" tabindex="-1" aria-hidden="true">
-                                        <option selected="" data-select2-id="2">My first option</option>
-                                        <option data-select2-id="16">Another option</option>
-                                        <option data-select2-id="17">Third option is here</option>
-                                    </select>
+                <span class="select2 select2-container select2-container--bootstrap4 select2-container--below select2-container--focus" dir="ltr" data-select2-id="1" style="width: 589.328px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-select01-container"><span class="select2-selection__rendered" id="select2-select01-container" role="textbox" aria-readonly="true" title="Third option is here">Third option is here</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
                 </div>
                 
                 <div class="tile-footer">
