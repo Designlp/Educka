dd4a22a418140ff04fc76d5e6a2456e7aaa7fefd
Jose <maytadazajos@correo.com>
2023-05-03 23:46:23 -0400

    Jose correcciones v4

--- Views/Template/Modals/modalcursos.php
+++ Views/Template/Modals/modalcursos.php
@@ -4,14 +4,6 @@
     <div class="modal-content">
       <div class="modal-header headerregister"> 
         <h5 class="modal-title" id="titlemodal">Nuevo Rol</h5>
-        <div class="form-group">
-                                    <label class="form-label" for="select01">Basic</label>
-                                    <select id="select01" data-toggle="select" class="form-control select2-hidden-accessible" data-select2-id="select01" tabindex="-1" aria-hidden="true">
-                                        <option selected="" data-select2-id="2">My first option</option>
-                                        <option data-select2-id="16">Another option</option>
-                                        <option data-select2-id="17">Third option is here</option>
-                                    </select><span class="select2 select2-container select2-container--bootstrap4 select2-container--below select2-container--focus" dir="ltr" data-select2-id="1" style="width: 589.328px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-select01-container"><span class="select2-selection__rendered" id="select2-select01-container" role="textbox" aria-readonly="true" title="Third option is here">Third option is here</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
-                                </div>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
