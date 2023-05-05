62ba5e3617fcf41799c7224e9ca0389bbd704e8b
Jose <maytadazajos@correo.com>
2023-05-05 10:25:34 -0400

    Jose Correcciones

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -52,7 +52,7 @@
 
 
 
-   <div class="mdk-drawer-layout js-mdk-drawer-layout" data-push data-responsive-width="992px">
+   <div class="" data-push data-responsive-width="992px">
       <div class="mdk-drawer-layout__content page-content">
 
          <!-- header-->
