7752fe6ab0266f78cf8d1f6c4ac8ee89be01075b
Jose <maytadazajos@correo.com>
2023-05-05 11:31:49 -0400

    Jose Correcciones

--- Assets/js/functions/functioncursos.js
+++ Assets/js/functions/functioncursos.js
@@ -109,7 +109,7 @@ function fnteditcurso(){
                         document.querySelector("#txtdescripcion").value=objdata.data.descripcion;
 
                         document.querySelector("#listpriv").value=objdata.data.privado;
-               
+                        alert(objdata.data.privado);
                         document.querySelector("#liststatus").value=objdata.data.estado;
                
                         
