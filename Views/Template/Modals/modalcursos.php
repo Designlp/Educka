6c51e4486860da0de59533f0d66a9d026c475361
Jose <maytadazajos@correo.com>
2023-05-05 10:45:51 -0400

    Jose Correcciones

--- Views/Template/Modals/modalcursos.php
+++ Views/Template/Modals/modalcursos.php
@@ -25,6 +25,13 @@
                 <textarea class="form-control" id="txtdescripcion" name="txtdescripcion" rows="2" placeholder="Descripcion de Rol"></textarea>
               </div>
 
+              <div class="form-group">
+                <label for="liststatus">Estado</label>
+                <select class="form-control" id="liststatus" name="liststatus">
+                  <option value="1">Activo</option>
+                  <option value="2">Inactivo</option>
+                </select>
+              </div>
 
               <div class="form-group">
                 <label for="listprivado">Disponible</label>
@@ -35,8 +42,8 @@
               </div>
 
               <div class="form-group">
-                <label class="form-label" for="liststatus">Basic</label>
-                <select id="liststatus" name="liststatus" data-toggle="select" class="form-control">
+                <label class="form-label" for="select01">Basic</label>
+                <select id="select01" data-toggle="select" class="form-control">
                   <option value="1">Activo</option>
                   <option value="2">Inactivo</option>
                 </select>
