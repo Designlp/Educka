5ae0c608f4585cba0ec9602468a97114e07e70f4
Jose <maytadazajos@correo.com>
2023-05-04 22:50:58 -0400

    Jose correciones

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -65,7 +65,8 @@
         <script src="<?= media() ?>/vendor/popper.min.js"></script>
 
      
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+      
         <!-- Perfect Scrollbar -->
         <script src="<?= media() ?>/vendor/perfect-scrollbar.min.js"></script>
 
