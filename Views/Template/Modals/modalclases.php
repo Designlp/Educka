057ed3a63a0a24dcffdfd5576b564c12d4079873
Jose <maytadazajos@correo.com>
2023-05-07 16:54:25 -0400

    Jose clases

--- Views/Template/Modals/modalclases.php
+++ Views/Template/Modals/modalclases.php
@@ -185,7 +185,7 @@
                                     
                                         <div class="plyr__video-embed" id="player">
                                                     <iframe class="embed-responsive-item"
-                                                    src="https://www.youtube.com/embed/G7mH-MBLET4?modestbranding=1&showinfo=0" 
+                                                    src="" 
                                                     title="Zoro Prueba el Poder de su Nueva Espada Yoru - One Piece" 
                                                     frameborder="0" 
                                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
