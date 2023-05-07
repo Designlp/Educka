40ad102268aa5ced0cf9f7237e20368f51d9b693
Jose <maytadazajos@correo.com>
2023-05-07 18:59:55 -0400

    Jose clases

--- Views/Template/footer_script.php
+++ Views/Template/footer_script.php
@@ -65,13 +65,12 @@ const player = new Plyr('#player', {
 
 player.on('pause', event => {
   if (event.detail.plyr.config.youtube) {
-
-    const youtubeWatermark = document.querySelector('.ytp-watermark');
-if (youtubeWatermark) {
-  youtubeWatermark.style.display = 'none';
-}
-// Ocultar el botón de pantalla completa y la marca de agua de YouTube
-    
+    setTimeout(() => {
+      const youtubeWatermark = document.querySelector('.ytp-watermark');
+      if (youtubeWatermark) {
+        youtubeWatermark.style.display = 'none';
+      }
+    }, 1000);
   }
 });
 
