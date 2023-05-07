b1f9baea16d381baef7875f685968a0c50ffe629
Jose <maytadazajos@correo.com>
2023-05-07 14:49:44 -0400

    Jose clases

--- prueba.php
+++ prueba.php
@@ -2,62 +2,42 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Video personalizado</title>
-    <script src="https://www.youtube.com/iframe_api?key=AIzaSyAY7t04ndWQXH4H34SHXBa8f2jSSOdaceU"></script>
-
-    <style>
-      #player {
-        width: 100%;
-        height: 0;
-        padding-bottom: 56.25%;
-        position: relative;
-      }
-      #video-overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: black;
-        opacity: 0.5;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        z-index: 1;
-      }
-      #play-button {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        background-color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-      }
-    </style>
+    <title>Video de YouTube personalizado</title>
   </head>
   <body>
     <div id="player"></div>
-    <div id="video-overlay">
-      <div id="play-button">&#9658;</div>
-    </div>
+
+    <script src="https://www.youtube.com/iframe_api"></script>
     <script>
       var player;
+
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          videoId: 'osUFRHYgvQ4',
+          videoId: 'HI8WFuTy4wc',
           playerVars: {
-      'origin': 'https://educka.online'
-    }
-         
+            'autoplay': 0,
+            'controls': 1,
+            'rel': 0,
+            'showinfo': 0,
+            'origin': 'https://educka.online'
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
         });
       }
+
       function onPlayerReady(event) {
-        document.getElementById('play-button').addEventListener('click', function() {
-          player.playVideo();
-          document.getElementById('video-overlay').style.display = 'none';
-        });
+        // Reproducir video cuando el reproductor esté listo
+        event.target.playVideo();
+      }
+
+      function onPlayerStateChange(event) {
+        // Detectar cambios de estado del reproductor (por ejemplo, cuando finaliza el video)
+        if (event.data == YT.PlayerState.ENDED) {
+          // Inserta aquí la acción que quieras que ocurra después de que finalice el video
+        }
       }
     </script>
   </body>
