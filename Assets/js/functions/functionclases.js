fce9c20f3240861cb37995a5a4abf89505886366
JoseM <maytadazajos@gmail.com>
2023-05-11 23:51:09 -0400

    Clases J

--- Assets/js/functions/functionclases.js
+++ Assets/js/functions/functionclases.js
@@ -67,8 +67,8 @@ function openmodal(){
     document.querySelector('#idcurso').value="";
     document.querySelector('#titlemodal').innerHTML = "Nuevo Usuario";
     document.querySelector('.modal-header').classList.replace("headerupdate","headerregister");
-    //document.querySelector('#btnactionform').classList.replace("btn-info","btn-primary");
-    //document.querySelector('#btntext').innerHTML="Guardar";
+    document.querySelector('#btnactionform').classList.replace("btn-info","btn-primary");
+    document.querySelector('#btntext').innerHTML="Guardar";
     document.querySelector('#formcurso').reset();
     $('#modalformcursos').modal("show");
     
