97c31b7078f871f274637cf00b8ddefa2daff5bd
JoseM <maytadazajos@gmail.com>
2023-05-11 23:48:48 -0400

    Clases J

--- Views/Template/Modals/modalclases.php
+++ Views/Template/Modals/modalclases.php
@@ -49,13 +49,13 @@
                             </div>
                             <div class="col-md-4">
 
-                                <div class="card">
-                                    <div class="card-header text-center">
-                                        <a href="#"
-                                           class="btn btn-accent">Guardar</a>
-                                    </div>
+                                <!-- <div class="card"> -->
+                                    <button class="card-header text-center btnactionform" type="submit">
+                                        <span 
+                                           class="btn btn-accent btntext">Guardar</span>
+                                    </button>
                                   
-                                </div>
+                                <!-- </div> -->
 
                                 <div class="page-separator">
                                     <div class="page-separator__text">Video</div>
