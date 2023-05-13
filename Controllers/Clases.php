8852761dd800bd76cbc4cc340121548047c51fd7
JoseM <maytadazajos@gmail.com>
2023-05-13 19:33:16 -0400

    Jose video 12

--- Controllers/Clases.php
+++ Controllers/Clases.php
@@ -86,7 +86,7 @@
             $strenlace=$_POST['txtenlace'];
             
             $intstatus=1;
-            
+            print $idcurso;
  
             if($intidclase == 0){
                  $requestrol=$this->model->insertclase($idcurso,$strtitulo, $strdescripcion,$strenlace,$intstatus);
