a4657fc88bb6e3933eecd2d4404a902e08883923
JoseM <maytadazajos@gmail.com>
2023-05-14 18:25:53 -0400

    Jose video

--- Views/Template/Modals/modalclases.php
+++ Views/Template/Modals/modalclases.php
@@ -53,7 +53,7 @@
                                             </div>
 
                                             <div class="card">
-                                                <div id="player"  class="embed-responsive embed-responsive-16by9 plyr__video-embed"  controls data-plyr-config='{ "controls": "[play-large]" }'>
+                                                <div id="player"  class="embed-responsive embed-responsive-16by9 plyr__video-embed"  controls data-plyr-config='{ "controls": [play-large] }'>
                                                     <iframe id="youtubeframeid"
                                                         class="embed-responsive-item" 
                                                         src="https://www.youtube.com/embed/3XozqbLvNx4?modestbranding=1&disablekb=1&rel=0&loop=1&mute=1&showinfo=0&controls=0&iv_load_policy=3" 
