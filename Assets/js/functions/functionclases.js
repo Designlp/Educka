6a4d836e1254354b8caee4f8b92996173a814429
JoseM <maytadazajos@gmail.com>
2023-05-14 22:03:32 -0400

    Jose video

--- Assets/js/functions/functionclases.js
+++ Assets/js/functions/functionclases.js
@@ -217,13 +217,14 @@ iframes.forEach(function (iframe) {
         disableYouTube: true, 
         youtube: {
             noCookie: true,
+            enablejsapi:1,
             rel: 0,
             modestbranding: 1,
             showinfo: 0,
             iv_load_policy: 3,
             controls:0,
             origin:'https://educka.online',
-            enablejsapi:1,
+         
             disablekb:1
           },
     });
