9da58dbb12bd8be68e8eeef864716bc31dd1de88
JoseM <maytadazajos@gmail.com>
2023-06-07 23:39:27 -0400

    Jose principal

--- Views/Login/cambiopassword.php
+++ Views/Login/cambiopassword.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Main CSS-->
-  <link rel="stylesheet" type="text/css" href="<?=media()?>/css/formstyle/main.css">
+  <link rel="stylesheet" type="text/css" href="<?=media()?>/css/main.css">
   <link rel="stylesheet" type="text/css" href="<?=media()?>/css/formstyle/style.css">
   <!-- Font-icon css-->
   <title></title>
