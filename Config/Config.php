3fb17575ebd2cb301e5ee213a58425a2afb50b8a
JoseM <maytadazajos@gmail.com>
2023-06-21 21:39:16 -0400

    Jose

--- Config/Config.php
+++ Config/Config.php
@@ -1,14 +1,21 @@
 <?php 
+    //Zona horaria
+    date_default_timezone_set('UTC');
+
 
     const BASE_URL = "https://educka.net";
-
-    //Zona horaria
-	date_default_timezone_set('UTC');
-    
-    const DB_HOST = "localhost";
     const DB_NAME = "u583014169_db_educkanet";
     const DB_USER = "u583014169_usereduckaADM";
     const DB_PASSWORD = "gtaV19921963";
+
+   
+    // const BASE_URL = "http://localhost/Educka-1";
+    // const DB_HOST = "localhost";
+    // const DB_NAME = "db_educkaonline";
+    // const DB_USER = "root";
+    // const DB_PASSWORD = "";
+
+
     const DB_CHARSET = "utf8";
 
     const NOMBRE_REMITENTE = "Educka";
