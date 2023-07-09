66dbf9da1d4398267e32050a596b382a294e6b3e
JoseM <maytadazajos@gmail.com>
2023-07-09 18:56:23 -0400

    Jose

--- Assets/js/functions/functionresetpassword.js
+++ Assets/js/functions/functionresetpassword.js
@@ -12,19 +12,9 @@ document.addEventListener("DOMContentLoaded",function () {
           swal("Por favor", "Escribe tu correo electrónico","error");
           return false;
         }else{
-          var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-          var ajaxUrl = baseurl + "/Login/resetpassword";
-          var formdata = new FormData(formreset);
-
-          request.open("POST", ajaxUrl, true);
-
-          request.send(formdata);
-          request.onreadystatechange = function(){
-              console.log(request);
-              if(request.readyState != 4) return;
-              if(request.status == 200){
-                var obdata=JSON.parse(request.responseText);
-                if(obdata.status){
+     
+            
+      
                     swal({
                       title:"Atencion",
                       text: obdata.msg,
@@ -35,20 +25,9 @@ document.addEventListener("DOMContentLoaded",function () {
                       closeOnConfirm:false,
                       closeOnCancel:true
                     },function(isConfirm){
-                        if(isConfirm){
-                            window.location= baseurl;
-                        }
+                    
                     });
-                }else{
-                  swal("Atencion",obdata.msg,"error");
-                }
               
-              }else{
-                swal("Atencion","error en el proceso","error");
-              }
-              return false;
-              
-          }
        
         }
       }
