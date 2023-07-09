90da579a1e174ae7ee8053bbafefb09085679081
JoseM <maytadazajos@gmail.com>
2023-07-09 19:07:23 -0400

    Jose V1

--- Models/ResetpasswordModel.php
+++ Models/ResetpasswordModel.php
@@ -3,6 +3,11 @@
     class ResetpasswordModel extends Mysql{
         //Nivel de accesos
     
+        public $intiduser;
+        public $stremail;
+        public $struser;
+        public $strpassword;
+        public $strtoken;
 
         public function __construct() {
 
@@ -16,6 +21,16 @@
             $sql= "SELECT idusuario , nombre, apellidos, estado FROM tusuarios WHERE correo='$this->struser' AND estado = 1";
             $request=$this->select($sql);
             return $request;
+        }
+
+        public function settokenuser(int $iduser, string $tokrn){
+            $this->intiduser = $iduser;
+            $this->strtoken= $tokrn;
+            $queryupdate="UPDATE tusuarios SET token = ? WHERE idusuario =$this->intiduser";
+            $arrdata = array($this->strtoken);
+            $requestupdate= $this->update($queryupdate,$arrdata);
+            return $requestupdate;
+                
         }
        
       
