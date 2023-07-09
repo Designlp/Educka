b10a13d590c7eb2008239636b9eace9981d9c205
JoseM <maytadazajos@gmail.com>
2023-07-08 21:53:44 -0400

    Jose

--- Controllers/Resetpassword.php
+++ Controllers/Resetpassword.php
@@ -9,7 +9,7 @@
             $data['page_tag'] = "Dashboard";
             $data['page_title']= "Dashboard";
             $data['page_name'] = "Dashboard";
-            $data['page_functions_js'] = "functionlogin.js";
+            $data['page_js'] = "functionlogin.js";
             session_start();
             $this->views->getview($this,"resetpassword",$data);
             
