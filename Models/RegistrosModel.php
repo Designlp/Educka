ec740ce5c3856d9a8eacc4dafbf54e6471a8539f
JoseM <maytadazajos@gmail.com>
2023-07-09 21:00:48 -0400

    Jose V1

--- Models/RegistrosModel.php
+++ Models/RegistrosModel.php
@@ -11,7 +11,6 @@
         private $intestado;
         private $inttelefono;
         private $intci;
-
         private $strpassword;
         private $intsuscripcion;
         private $strtoken;
@@ -20,27 +19,7 @@
 
             parent::__construct();
         }
-        //YO
-        public function selectdocentes(){
-            $sql= "SELECT tu.idusuario, tu.idroles, tu.nombre, tu.apellidos, tu.correo, tu.telefono,tu.password,tu.suscripcion, tu.estado 
-            FROM tusuarios tu 
-            JOIN troles tr ON tu.idroles = tr.idroles 
-            WHERE tu.estado != 0 AND tr.estado = 1";
-            $request=$this->selectall($sql);
-            return $request;
-        }
-
-        public function selectdocente(int $iduser){
-            $this->intidusuario= $iduser;
-            $sql= "SELECT tu.idusuario, tu.ci, tu.nombre, tu.apellidos, tu.correo, tu.telefono, tu.estado
-            FROM tusuarios tu
-            WHERE tu.idusuario = $this->intidusuario";
-            $request=$this->select($sql);
-            return $request;
-        }
-
-
-        public function insertdocentes(int $idrol,string $ci, string $nombre, string $apellido, string $email, int $telefono, string $suscripcion ,string $password, int $estado){
+        public function insertregistro(int $idrol,string $ci, string $nombre, string $apellido, string $email, int $telefono, string $suscripcion ,string $password, int $estado){
             $this->intidrol = $idrol;
 			$this->intci = $ci;
 			$this->strnombre = $nombre;
@@ -74,49 +53,12 @@
 	        	$request = $this->insert($query,$arrdata);
 	        	$return = $request;
 			}else{
-                $return=-1; 
+                $return=1; 
             }
             
             return $return;
         }
-        //Update
-        public function updatedocentes(int $idusuario,string $ci,string $nombre, string $apellido, string $correo ,int $telefono, int $estado){
-			
-            $this->intidusuario = $idusuario;
-			$this->strci        = $ci;
-			$this->strnombre    = $nombre;
-			$this->strapellido  = $apellido;
-            $this->strcorreo = $correo;
-			$this->inttelefono  = $telefono;
-            $this->intestado     = $estado;
-            
-
-            $sql= "SELECT * FROM tusuarios WHERE nombre='{$this->strnombre}' AND apellidos='{$this->strapellido}' AND idusuario != $this->intidusuario";
-            $requestupdate = $this->selectall($sql);
-            
-            if(empty($requestupdate)){
-
-                    $queryupdate="UPDATE tusuarios SET ci=?, nombre=?, apellidos=?, correo=?,telefono=?,estado=? WHERE idusuario=$this->intidusuario";
-                    $arrdata = array(
-        						$this->strci,
-        						$this->strnombre,
-                                $this->strapellido,
-        						$this->strcorreo,
-        						$this->inttelefono,
-                                $this->intestado
-                                //Cuidao al borrar
-                    );
-                $requestupdate= $this->update($queryupdate,$arrdata);
-                $return=$requestupdate;
-                
-            }else{
-                $return=-1;
-            }
-            
-            return $return;
-
-        }
-   
+ 
         public function settokenuser(int $iduser, string $token){
             $this->intidusuario = $iduser;
             $this->strtoken= $token;
