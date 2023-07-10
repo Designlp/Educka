afbf30baae4729cb5deaa777e6a491c7b315d4fa
JoseM <maytadazajos@gmail.com>
2023-07-09 20:42:45 -0400

    Jose V1

--- Assets/js/functions/functionregistros.js
+++ Assets/js/functions/functionregistros.js
@@ -7,7 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
     forminsert.onsubmit = function (e) {
         e.preventDefault();
 
-alert();
+        var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        var ajaxUrl = baseurl + '/Registros/setregistros';
+        var formdata = new FormData(forminsert);
+        request.open("POST", ajaxUrl, true);
+        request.send(formdata);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+
+                var obdata = JSON.parse(request.responseText);
+
+                if (obdata.status) {
+
+                    swal("Usuario Creado", obdata.msg, "success");
+                } else {
+                    swal("Error", obdata.msg, "error");
+
+                }
+            }
+        }
     }
 
 }, false);
